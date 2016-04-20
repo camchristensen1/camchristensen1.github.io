037ceae437bbd030feab99fbaebd4f79655501a6
Nick Christensen <nick.christensen@firespring.com>
2016-04-20 10:27:02 -0500

    folder setup

---
+++ js/site.js
@@ -0,0 +1,25 @@
+// =============================================================================
+//	Imports
+// =============================================================================
+//= include components/updatevalue.js
+//= include components/backgroundImage.js
+//= include components/bodyClassToggler.js
+//= include components/placeholders.js
+//= include components/accordianMenu.js
+//= include components/navChecker.js
+//= include components/textWrapper.js
+//= include components/fireslider_settings.js
+//= include components/landing_page.js
+
+
+
+$(document).ready(function() {
+
+	backgroundImage($(".has-bg"), "center center / cover no-repeat", 2);
+	backgroundImage($(".has-bg__collection li"), "center center / cover no-repeat", 2);
+	backgroundImage($(".has-bg-li__collection li .collection-item-image"), "center center / cover no-repeat", 1);
+	
+  placeHolders('form');
+  $('header .wrap').navChecker();
+
+}); // end document ready
